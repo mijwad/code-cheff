601d1fbbdfe7ababb97db3283fe7791a73950170
mijwad <mijwadvkd@gmail.com>
2026-01-11 06:34:16 +0530

    logo picture updtate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
+
+import chefIcon from "../assets/chef-claude-icon.png"
+
 export default function Header() {
-    return (
-        <>
-            <header className="header">
-                <img src="/src/assets/chef-claude-icon.png" />
-                <h1>Chef Nida</h1>
-            </header>
-        </>
-    )
+  return (
+    <header className="header">
+      <img src={chefIcon} alt="Chef Nida" />
+      <h1>Chef Nida</h1>
+    </header>
+  )
 }
